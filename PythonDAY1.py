5ae948a19ed0b80a436dc88264ee5b1ff074d2b5
Roy3848 <100965747+Roy3848@users.noreply.github.com>
2022-03-05 01:40:19 +0530

    Add files via upload

---
+++ PythonDAY1.py
@@ -0,0 +1,10 @@
+company_name = "Infosys"
+
+def my_company():
+    global company_name
+    company_name = "Mindbowser"
+    print("I am an employee at " + company_name)
+
+my_company()
+
+print("I am an employee at " + company_name)
